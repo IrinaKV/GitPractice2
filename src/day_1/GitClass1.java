c800d90762b948e40ccb112e0881afb6a72155ff
IrinaKV <61922863+IrinaKV@users.noreply.github.com>
2020-04-24 16:29:12 -0400

    Update GitClass1.java
    add changes

--- src/day_1/GitClass1.java
+++ src/day_1/GitClass1.java
@@ -3,5 +3,7 @@ package day_1;
 public class GitClass1 {
     public static void main(String[] args) {
         System.out.println("My practice");
+        System.out.println("My changes");
+        
     }
 }
