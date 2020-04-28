cae2c339f281c1dabdd7c8221d9c6a30ca72348d
IrinaKV <61922863+IrinaKV@users.noreply.github.com>
2020-04-28 15:56:48 -0400

    Update HappyBirthday.java

--- src/day_1/HappyBirthday.java
+++ src/day_1/HappyBirthday.java
@@ -4,6 +4,7 @@ public class HappyBirthday {
     public static void main(String[] args) {
         // HappyBirthday
         System.out.println("Hi, HappyBirthday");
+        System.out.println("Hi, HappyBirthday");
             
     }
 }
