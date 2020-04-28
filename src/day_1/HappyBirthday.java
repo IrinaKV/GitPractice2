aaa9a8e47742dd0b1144af7e9726463fb9d24166
IrinaKV <61922863+IrinaKV@users.noreply.github.com>
2020-04-28 15:48:42 -0400

    add "System.out.println("Hi, HappyBirthday");"

--- src/day_1/HappyBirthday.java
+++ src/day_1/HappyBirthday.java
@@ -3,5 +3,7 @@ package day_1;
 public class HappyBirthday {
     public static void main(String[] args) {
         // HappyBirthday
+        System.out.println("Hi, HappyBirthday");
+            
     }
 }
